d0d4eefe09414119a0a08c35c836d19425998307
Nidhi <nidhi9804@gmail.com>
2023-03-16 15:01:37 +0530

    second commit

--- src/component/NewsItem.js
+++ src/component/NewsItem.js
@@ -12,9 +12,9 @@ export  class NewsItem extends Component {
                     <div className="card-body">
 
                         <h5 className="card-title">{Title}</h5> 
-                        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                        <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
     {source}
-    <span class="visually-hidden">unread messages</span>
+    <span className="visually-hidden">unread messages</span>
   </span>
                         <p className="card-text">{description}</p>
                         <p className="card-text"><small className="text-muted">By {!author ? "unknown" : author } on {date}</small></p>
